85adf3ae28a6085f25d912258f8fdd8b73b6fe62
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-05-08 20:37:31 +0300

    chore - designs

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -29,7 +29,7 @@ const Steps = () => {
     return (
         <ol className="rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
             {STEPS.map((step, i) => {
-                const isCurrent = pathname.endsWith(steps.url);
+                const isCurrent = pathname.endsWith(step.url);
                 const isCompleted = STEPS.slice(i+1).some(step=>pathname.endsWith(step.url));
                 const imgPath = `/snake-${i+1}.png`;
 
@@ -46,9 +46,29 @@ const Steps = () => {
                                 <img src={imgPath} className={cn("flex h-20 w-20 object-contain items-center justify-center", {
                                     "border-none": isCompleted,
                                     "border-zinc-700": isCurrent,
-                                })} alt="" />
+                                })} />
+                            </span>
+                            <span className="ml-4 h-full mt-0.5 flex min-w-0 flex-col justify-center">
+                                <span className={cn("text-sm font-semibold text-zinc-700", {
+                                    "text-primary": isCompleted,
+                                    "text-zinc-700": isCurrent,
+                                })}>{step.name}</span>
+                                <span className="text-sm text-zinc-500">{step.description}</span>
                             </span>
                         </span>
+                        {i!==0 ? <div className="absolute inset-0 hidden w-3 lg:block">
+                        <svg
+                            className='h-full w-full text-gray-300'
+                            viewBox='0 0 12 82'
+                            fill='none'
+                            preserveAspectRatio='none'>
+                            <path
+                                d='M0.5 0V31L10.5 41L0.5 51V82'
+                                stroke='currentcolor'
+                                vectorEffect='non-scaling-stroke'
+                            />
+                        </svg>
+                        </div> : null}
                     </div>
                 </li>
             })}
