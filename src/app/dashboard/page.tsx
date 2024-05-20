c2cf6e5546670bfbbede559a2a5bbc66b8a30051
Moldovan Dennis <dennismoldovan32@gmail.com>
2024-05-20 22:01:39 +0300

    chore - dash page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,40 @@
-const Page = () =>{
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
+
+const Page = async () =>{
     
+    const {getUser} = getKindeServerSession();
+
+    const user = await getUser();
+
+    const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+
+    if(!user || user.email!==ADMIN_EMAIL) {
+        return notFound();
+    }
+
+    const orders = await db.order.findMany({
+        where: {
+            isPaid: true,
+            createdAt: {
+                gte: new Date(new Date().setDate(new Date().getDate()-7))
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            user: true,
+            shippingAddress: true,
+        },
+    });
+
+    return (
+    <div className="flex min-h-screen w-full bg-muted/40">
+
+    </div>
+    )
 }
 
 export default Page;
